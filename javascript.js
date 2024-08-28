2cf3265e95b1b106dedf378f840204e546a7210e
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-08-28 17:34:56 -0400

    Implement playRound() and playGame()
    These are pretty logical and don't need to much
    explanation. The sentinelValue breaks the
    gameplay loop. I also had some syntax errors
    with function calls but I sorted it out.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,66 @@
-for (i = 0; i != -99; i++) {
-    
+function playGame() {
+    while (sentinelValue != -99) {
+        playRound(getHumanChoice(), getComputerChoice());
+        sentinelValue++;
+        if (sentinelValue % 5 == 0) {
+            let response = prompt("Keep Playing? (Y/n):");
+            response = response.toLowerCase();
+
+            if (response == "n" || response == "no") {
+                sentinelValue= -99;
+            }
+        }
+    }
 }
 
+let humanChoice = null;
 let odds = null;
 let computerChoice = null;
-let humanChoice = null;
 let upper = null;
-let sentinelValue = null;
 let promptEscape = null;
 let humanScore = 0;
 let computerScore = 0;
+let sentinelValue = 0;
 
 function getComputerChoice() {
     odds = (Math.random() * 100) + 1;
-    if (odds < 33) {
+    if (odds <= 33) {
        computerChoice = "Rock"; 
-    } else if (odds > 33 && odds < 66) {
+    } else if (odds > 33 && odds <= 66) {
         computerChoice = "Paper";
     } else {
         computerChoice = "Scissors";
     }
     odds = null;
+    return computerChoice;
 }
 
 function getHumanChoice() {
-    for (i = 0; i != -1; i++) {
+    while (true) {
         humanChoice = prompt("Enter Rock, Paper, or Scissors: ");
-        upper = humanChoice.charAt(0).toUpperCase;
-        humanChoice.slice(0, 1);
-        humanChoice = upper + humanChoice;
+        humanChoice = humanChoice.toLowerCase();
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
 
         if (humanChoice === "Rock" || humanChoice === "Paper" || humanChoice === "Scissors") {
-            i = -1;
+            return humanChoice;
         } else {
             console.log("Invalid input, please enter another choice.\n");
         }
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "Rock" && computerChoice == "Paper" || 
+        humanChoice == "Paper" && computerChoice == "Scissors" || 
+        humanChoice == "Scissors" && computerChoice == "Rock") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    } else if (humanChoice == computerChoice) {
+        console.log(`A tie! Both chose ${computerChoice}!`);
+    } else {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+        humanScore++;
+    }
+}
+
+playGame();
